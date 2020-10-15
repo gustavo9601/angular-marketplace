b88af8fb0076a652544e9bfd0decea4410893f01
gustavo9601 <tavo9601@hotmail.com>
2020-10-15 01:49:20 -0500

    Inicio slide de top 20 best seller

--- src/app/pages/home/home-hot-today/home-hot-today.component.ts
+++ src/app/pages/home/home-hot-today/home-hot-today.component.ts
@@ -3,6 +3,7 @@ import {ConfigService} from '../../../services/config.service';
 import {ProductsService} from '../../../services/products.service';
 
 import {carouselNavigation, countDown, owlCarouselConfig, productLightbox, progressBar, rating, slickConfig} from '../../../utilities';
+import {SalesService} from '../../../services/sales.service';
 
 @Component({
   selector: 'app-home-hot-today',
@@ -13,49 +14,25 @@ export class HomeHotTodayComponent implements OnInit {
 
 
   productsAvailableStockPromotion: Array<any>;
+  productsInfo: Array<any>;
+  productsBestSeller: Array<any>;
   render: boolean;
   preload: boolean;
 
   constructor(public _configService: ConfigService,
-              private _productService: ProductsService) {
+              private _productService: ProductsService,
+              private _salesService: SalesService) {
     this.productsAvailableStockPromotion = [];
+    this.productsInfo = [];
+    this.productsBestSeller = [];
     this.render = true;
     this.preload = false;
   }
 
   ngOnInit(): void {
 
+    this.getProductsBanners();
 
-    const hoy = new Date();
-    this.preload = true;
-    this._productService.getData().subscribe(
-      (response) => {
-        // Recorrer los productos para obtener solo los que tienen ofertas y tienen stock
-        const products = Object.values(response);
-
-        // Parseando en el arreglo el indice offer a array
-        products.map((product: any) => {
-          product.offer = JSON.parse(product.offer);
-          product.offer[1] = Number(product.offer[1]);
-          product.gallery = JSON.parse(product.gallery);
-          product.reviews = JSON.parse(product.reviews);
-
-          product.calculate_review = this.calculateReview(product.reviews);
-
-          product.offer[2] = new Date(Date.parse(product.offer[2]));  // parseando el string a un tipo fecha
-        });
-
-
-        // Retornando los productos filtrados que no tengan caucada la promocin y con stock
-        this.productsAvailableStockPromotion = products.filter((product: any) => {
-          return product.offer[2] >= hoy && product.stock > 0;
-        });
-
-        this.preload = false;
-        console.log('this.productsAvailableStockPromotion', this.productsAvailableStockPromotion);
-
-      }
-    );
 
   }
 
@@ -81,6 +58,73 @@ export class HomeHotTodayComponent implements OnInit {
       rating();
       progressBar();
     }
+  }
+
+  getProductsBanners() {
+
+    const hoy = new Date();
+    this.preload = true;
+    this._productService.getData().subscribe(
+      (response) => {
+        // Recorrer los productos para obtener solo los que tienen ofertas y tienen stock
+        const products = Object.values(response);
+        // Variable que se usara para hacer el match con los products bes seller
+        this.productsInfo = products;
+
+        // Parseando en el arreglo el indice offer a array
+        products.map((product: any) => {
+          product.offer = JSON.parse(product.offer);
+          product.offer[1] = Number(product.offer[1]);
+          product.gallery = JSON.parse(product.gallery);
+          product.reviews = JSON.parse(product.reviews);
+
+          product.calculate_review = this.calculateReview(product.reviews);
+
+          product.offer[2] = new Date(Date.parse(product.offer[2]));  // parseando el string a un tipo fecha
+        });
+
+
+        // Retornando los productos filtrados que no tengan caucada la promocin y con stock
+        this.productsAvailableStockPromotion = products.filter((product: any) => {
+          return product.offer[2] >= hoy && product.stock > 0;
+        });
+
+        this.preload = false;
+        console.log('this.productsAvailableStockPromotion', this.productsAvailableStockPromotion);
+
+        // Obteniendo los poructos vendidos
+        this.getSales();
+
+      }
+    );
+  }
+
+  getSales() {
+    this._salesService.getData().subscribe(
+      (responseSales) => {
+
+        const productsSales = Object.values(responseSales);
+
+        // Ordenando arreglo por indice quantity de mayor a menor
+        productsSales.sort((a, b) => {
+          return b.quantity - a.quantity;
+        });
+
+        // Filtrando valores unicos del array
+        const unicProductsSales = productsSales.filter((valorActual, indiceActual, arreglo) => {
+          return arreglo.findIndex(valorDelArreglo => JSON.stringify(valorDelArreglo) === JSON.stringify(valorActual)) === indiceActual;
+        });
+
+
+        // Asiendo el buscado sobre el arreglo de products, y pusehando en el arreglo global productsSales
+        unicProductsSales.forEach((productSale) => {
+          this.productsBestSeller.push(this.productsInfo.find((productInfo) => {
+            return productSale.product === productInfo.name;
+          }));
+        });
+
+      }
+    );
   }
 
 
